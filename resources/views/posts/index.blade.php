bc34873b1e30dd528dea940b6062cb3a82537f35
MohamedYehia599 <mohamed.yehia3331@gmail.com>
2022-04-19 05:11:50 +0200

    lab1_edit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,7 +32,7 @@ index
       <td>{{$post['date']}}</td>
       <td><div class="d-flex justify-content-start">
     <a class="btn btn-info mx-1" href="{{route('posts.show',['post'=>$post['id']])}}">View</a>
-    <a class="btn btn-primary mx-1" href="">Edit</a>
+    <a class="btn btn-primary mx-1" href="{{route('posts.edit',['post'=>$post['id']])}}">Edit</a>
     <a class="btn btn-danger mx-1" href="">Delete</a>
 
 
